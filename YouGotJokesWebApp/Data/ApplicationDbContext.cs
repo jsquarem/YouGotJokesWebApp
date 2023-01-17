ead90ef67aa07a499eed7c2e4c68abcea0e8d797
Jeff Martin <jeffjmart@gmail.com>
2023-01-17 16:00:31 -0600

    Initial push JokeWebApp built in ASP.NET

---
+++ YouGotJokesWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YouGotJokesWebApp.Models;
+
+namespace YouGotJokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<YouGotJokesWebApp.Models.Joke> Joke { get; set; }
+    }
+}
